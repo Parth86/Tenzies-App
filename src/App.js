c0ca2e21cb3f35cb13fc4dae0365daab32dfe282
Parth86 <60189466+Parth86@users.noreply.github.com>
2022-05-01 15:33:11 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Die from './Die.js';
+import React, { useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 function App() {
+  if(!localStorage.getItem('bestTurn')) localStorage.setItem('bestTurn', 100)
+
+  function getNums() {
+    let nums = Array(10).fill({})
+    for(let i = 0; i < nums.length; i++) {
+      const rand = Math.floor(Math.random() * 6) + 1
+      nums[i] = {value: rand, isHeld: false, id: nanoid()}
+    }
+    return nums
+  }
+
+  function rollDice() {
+    if(!tenzies){
+      setDice(oldDice => {
+        const newDice = getNums()
+        return oldDice.map((die, index) => die.isHeld ? die : newDice[index])
+      })
+      setTurns(turns + 1)
+    } else {
+      setTenzies(false)
+      setDice(getNums())
+      if(turns < bestScore) {
+        setBestScore(turns); localStorage.setItem('bestTurn', turns)
+      }
+      setTurns(0)
+    }
+    
+    }
+    
+
+  function changeHold(id) {
+    setDice(dice => dice.map(die => {
+      if(die.id === id) return {
+        ...die, isHeld: !die.isHeld
+      }
+      else return die
+    })) 
+  }
+  const [bestScore, setBestScore] = React.useState(() => localStorage.getItem('bestTurn'))
+  const [dice, setDice] = React.useState(getNums())
+  const [tenzies, setTenzies] = React.useState(false)
+  const [turns, setTurns] = React.useState(0)
+
+  useEffect(() => {
+    if(dice.every(die => die.isHeld === true && die.value === dice[0].value)) setTenzies(true)
+  }, [dice])
+
+  const diceElements = dice.map(die => <Die value={die.value} isHeld={die.isHeld} onClick={() => changeHold(die.id)} />)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Tenzies</h1>
+      <h2>Best Score: {bestScore}</h2>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className='dies'>
+        {diceElements}
+      </div>
+      <button onClick={rollDice} className="roll-dice" > {tenzies ? "New Game" : "Roll"} </button>
+      <h3>Turns: {turns} </h3>
+      {tenzies && <Confetti />}
+    </main>
   );
 }
 
